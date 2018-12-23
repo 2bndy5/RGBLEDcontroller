8754c4fe9cd379805b2b34d22439a9b629c64287
2bndy5 <2bndy5@gmail.com>
2018-12-23 14:37:58 -0800

    fixed typo in main

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from mcp300x import ADC
 from colour import colour
-from gpiozero import RGBLED, MCP3008
-import time
+from gpiozero import RGBLED
 
 strip = RGBLED(13, 6, 5)
 c = colour()
 adc = ADC(0)
-#hPot = MCP3008(channel=0)
-#iPot = MCP3008(channel=1)
 
 while True:
     try:
@@ -15,9 +12,8 @@ while True:
         i = adc.mcp3008(1)
         c.setH(round(h / 1023.0 * 360))
         c.setI(i / 1023.0)
-        print("i_pot =", c.intensity, " h_pot =", c.hue, "\nRGB =", c.red, c.green, c.blue)
+        #print("i_pot =", c.intensity, " h_pot =", c.hue, "\nRGB =", c.red, c.green, c.blue)
         strip.color = (c.red / 255.0, c.green / 255.0, c.blue / 255.0)
-        time.sleep(0.5)
     except KeyboardInterrupt:
         break
 #end infinite loop
