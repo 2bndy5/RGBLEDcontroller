94b1f5165fdf312565d6c686b0522650547a7fd9
Brendan <2bndy5@gmail.com>
2019-02-09 18:09:34 -0800

    cleaned ADC class, fixed binary output on pigpio.spi_xfer args

--- mcp300x.py
+++ mcp300x.py
@@ -6,49 +6,48 @@ class ADC:
     """
     def __init__(self, CS):
         self.pi = pigpio.pi()
-        self.cs = 0
-        if CS > 1 or CS < 0:
-            self.cs = 0
-        else: self.cs = CS
+        self.cs = max(0, min(1, CS))
 
     def mcp3002(self, channel):
         # check proper range for analog channel (0 or 1 on MCP3002)
-        if channel > 1 or channel < 0:
-            channel = 0
+        channel = max(0, min(1, channel))
         # create handle for use with pigpio spi functions
         self.adc = self.pi.spi_open(self.cs, 50000)
         # gather data after sending data. *see spi_xfer(args)
-        self.result = self.pi.spi_xfer(self.adc, [1, (2 + channel) << 6, 0])
-        # for debug purposes
-        # print(self.result)
-        # for i in range(self.result[0]):
-        #     print(bin(self.result[1][i]), end = ' ')
+        # binary data output format = 1(channel in 1 bit)0
+        result = self.pi.spi_xfer(self.adc, ((2 | channel) << 1))
         # now delete handle
         self.pi.spi_close(self.adc)
         # return data from chip
-        return (self.result[1][1] << 8) | self.result[1][2]
-    # end adc2
+        return (result[1][1] << 8) | result[1][2]
 
+    def mcp3004(self, channel):
+        # check proper range for analog channel (0 to 3 on MCP3004)
+        channel = max(0, min(3, channel))
+        # create handle for use with pigpio spi functions
+        self.adc = self.pi.spi_open(self.cs, 50000)
+        # gather data after sending data. *see spi_xfer(args)
+        # binary data output format = 1(channel in 3 bits)0
+        result = self.pi.spi_xfer(self.adc, ((8 | channel) << 1))
+        # now delete handle
+        self.pi.spi_close(self.adc)
+        # return data from chip
+        return (result[1][1] << 8) | result[1][2]
+    
     def mcp3008(self, channel):
         # check proper range for analog channel (0 to 7 on MCP3008)
-        if channel > 7 or channel < 0:
-            channel = 0
+        channel = max(0, min(7, channel))
         # create handle for use with pigpio spi functions
         self.adc = self.pi.spi_open(self.cs, 50000)
         # gather data after sending data. *see spi_xfer(args)
-        self.result = self.pi.spi_xfer(self.adc, [1, (2 + channel) << 6, 0])
-        # for debug purposes
-        # print(self.result)
-        # for i in range(self.result[0]):
-        #     print(bin(self.result[1][i]), end = ' ')
+        # binary data output format = 1(channel in 3 bits)0
+        result = self.pi.spi_xfer(self.adc, ((8 | channel) << 1))
         # now delete handle
         self.pi.spi_close(self.adc)
         # return data from chip
-        return (self.result[1][1] << 8) | self.result[1][2]
-
-    # end adc8
+        return (result[1][1] << 8) | result[1][2]
     
     def __del__(self):
         self.pi.stop()
-        del self.pi, self.cs
+        del self.pi, self.cs, result
 # end class ADC
