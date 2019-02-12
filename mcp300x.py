abebdff977a5ff7043aff16e650b3ee89db9e241
Brendan <2bndy5@gmail.com>
2019-02-12 00:11:39 -0800

    changed bus variable to be object specific

--- mcp300x.py
+++ mcp300x.py
@@ -5,7 +5,6 @@ class ADC:
     """
     class for gathering data from MCP300x IC via SPI
     """
-    bus = 0 # default is 0, but can be 1 if user enabled second bus
     def __init__(self, CS):
         self.cs = max(0, min(1, CS))
         GPIO.setmode(GPIO.BCM)
@@ -16,12 +15,13 @@ class ADC:
         GPIO.setup(8 - self.cs, GPIO.OUT)   # CS pin (CE0 | CE1) -> CE == 0 ? 8 : 7
         # create object for handling SPI interface with spidev
         self.spi = spidev.SpiDev()
+        self.bus = 0 # default is 0, but can be 1 if user enabled second bus
 
     def mcp3002(self, channel):
         # check proper range for analog channel (0 or 1 on MCP3002)
         channel = max(0, min(1, channel))
         # open a handle to the SPI bus using the CS
-        self.spi.open(bus, self.cs)
+        self.spi.open(self.bus, self.cs)
         # set CLK speed to 50kHz according to datasheet
         self.spi.max_speed_hz = 50000
         # spidev docs mandate CS pin pulled low during xfer2() and then high afterward
@@ -39,7 +39,7 @@ class ADC:
         # check proper range for analog channel (0 to 3 on MCP3004)
         channel = max(0, min(3, channel))
         # open a handle to the SPI bus using the CS
-        self.spi.open(bus, self.cs)
+        self.spi.open(self.bus, self.cs)
         # set CLK speed to 50kHz according to datasheet
         self.spi.max_speed_hz = 50000
         # spidev docs mandate CS pin pulled low during xfer2() and then high afterward
@@ -57,7 +57,7 @@ class ADC:
         # check proper range for analog channel (0 to 7 on MCP3008)
         channel = max(0, min(7, channel))
         # open a handle to the SPI bus using the CS
-        self.spi.open(bus, self.cs)
+        self.spi.open(self.bus, self.cs)
         # set CLK speed to 50kHz according to datasheet
         self.spi.max_speed_hz = 50000
         # spidev docs mandate CS pin pulled low during xfer2() and then high afterward
@@ -73,7 +73,7 @@ class ADC:
     
     def __del__(self):
         self.spi.close() # in case of KeyboardInterrupt
-        del self.cs, self.spi
+        del self.cs, self.spi, self.bus
 # end class ADC
 
 if __name__ == "__main__":
