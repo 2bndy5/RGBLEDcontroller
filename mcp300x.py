eb8644e1014513241cc5814acc4bb53e6190f198
Brendan <2bndy5@gmail.com>
2019-02-12 00:57:46 -0800

    still debugging

--- mcp300x.py
+++ mcp300x.py
@@ -59,15 +59,13 @@ class ADC:
         channel = max(0, min(7, channel))
         # open a handle to the SPI bus using the CS
         self.spi.open(self.bus, self.cs)
-        # set CLK speed to 50kHz according to datasheet
-        self.spi.max_speed_hz = 50000
         # spidev docs mandate CS pin pulled low during xfer2() and then high afterward
-        GPIO.output((8 - self.cs), False)
+        # GPIO.output((8 - self.cs), False)
         # gather data after sending data. *see spi_xfer2(args)
         # data output format = [1, (channel in 3 bits shifted to 8 bit length), 0]
-        result = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        result = self.spi.xfer2([1, (8 + channel) << 4, 0], 50000)
         print('result =', result)
-        GPIO.output((8 - self.cs), True)
+        # GPIO.output((8 - self.cs), True)
         # now delete handle
         self.spi.close()
         # return data from chip (last 10 bits of resulting bytearray)
