d16fd44191f04d8f999db9f12bb9a41a071cef54
Brendan <2bndy5@gmail.com>
2019-02-12 01:30:02 -0800

    SPI handle is open during entire life of object

--- mcp300x.py
+++ mcp300x.py
@@ -1,5 +1,4 @@
 import spidev
-# import RPi.GPIO as GPIO
  
 class ADC:
     """
@@ -7,54 +6,36 @@ class ADC:
     """
     def __init__(self, CS):
         self.cs = max(0, min(1, CS))
-        # # GPIO.setwarnings(False)
-        # GPIO.setmode(GPIO.BCM)
-        # # set up the SPI interface pins
-        # GPIO.setup(10, GPIO.OUT)            # MOSI pin
-        # GPIO.setup(9, GPIO.IN)              # MISO pin
-        # GPIO.setup(11, GPIO.OUT)            # CLK pin
-        # GPIO.setup((8 - self.cs), GPIO.OUT)   # CS pin (CE0 | CE1) -> CE == 0 ? 8 : 7
-        # create object for handling SPI interface with spidev
         self.spi = spidev.SpiDev()
         self.bus = 0 # default is 0, but can be 1 if user enabled second bus
+        # open a handle to the SPI bus using the CS
+        self.spi.open(self.bus, self.cs)
 
     def mcp3002(self, channel):
         # check proper range for analog channel (0 or 1 on MCP3002)
         channel = max(0, min(1, channel))
-        # open a handle to the SPI bus using the CS
-        self.spi.open(self.bus, self.cs)
         # gather data after sending data. *see spi_xfer2(args)
         # data output format = [1, (channel in 1 bits shifted to 8 bit length), 0]
         result = self.spi.xfer2([1, (2 + channel) << 6, 0], 50000)
-        # now delete handle
-        self.spi.close()
         # return data from chip (last 10 bits of resulting bytearray)
         return ((result[1] & 3) << 8) + result[2]
 
     def mcp3004(self, channel):
         # check proper range for analog channel (0 to 3 on MCP3004)
         channel = max(0, min(3, channel))
-        # open a handle to the SPI bus using the CS
-        self.spi.open(self.bus, self.cs)
         # gather data after sending data. *see spi_xfer2(args)
         # data output format = [1, (channel in 3 bits shifted to 8 bit length), 0]
         result = self.spi.xfer2([1, (8 + channel) << 4, 0], 50000)
-        # now delete handle
-        self.spi.close()
         # return data from chip (last 10 bits of resulting bytearray)
         return ((result[1] & 3) << 8) + result[2]
     
     def mcp3008(self, channel):
         # check proper range for analog channel (0 to 7 on MCP3008)
         channel = max(0, min(7, channel))
-        # open a handle to the SPI bus using the CS
-        self.spi.open(self.bus, self.cs)
         # gather data after sending data. *see spi_xfer2(args)
         # data output format = [1, (channel in 3 bits shifted to 8 bit length), 0]
         result = self.spi.xfer2([1, (8 + channel) << 4, 0], 50000)
         print('result =', result)
-        # now delete handle
-        self.spi.close()
         # return data from chip (last 10 bits of resulting bytearray)
         return ((result[1] & 3) << 8) + result[2]
     
