1826023eafd28a3ae140f37af0efd853b3d226ae
Brendan <2bndy5@gmail.com>
2019-06-29 15:55:09 -0700

    need to finish driver portion of visual chimes

--- VisualChimes/Ripple.py
+++ VisualChimes/Ripple.py
@@ -1,10 +1,13 @@
+import time, math
+
+idleHSI = [0.0, 0.0, 0.5]
 
 class Ripple:
     def __init__(self, start, segments = 12):
         self.MaxStages = segments / 2
         self._it = 1
         self.start = start
-        self.wave = []
+        self.wave = [] # end points of ripple
     
     def fx(self):
         if 0 < self._it <= self.MaxStages:
@@ -23,6 +26,10 @@ class fxQ:
         self._q = []
         self.maxSize = maxSize
 
+    def __getattr__(self, key):
+        assert 0 <= key < len(self._q)
+        return self._q[key]
+
     def isIn(self, x):
         if 0 <= x <= self.maxSize:
             for i in range(len(self._q)):
@@ -39,3 +46,36 @@ class fxQ:
         self.pop(x)
         self._q.append(Ripple(x, self.maxSize))
     
+class Pixel:
+    def __init__(self, hsi = idleHSI, rampTime = 1000):
+        self.dt = rampTime
+        assert len(hsi) == 3
+        self.hsi = hsi
+        self.last_hsi = hsi # used to remember beginning of transition
+    
+    def cycleHue(self):
+        self.iniHue = int(time.monotonic() * 1000)
+        self.endHue = self.iniHue + self.dt
+        self.endNorm = self.endHue + self.dt
+        # set Hue = 0.0, Saturation = 1.0, & Intensity = idle intensity if idle is not off else full 
+        self.hsi = [0.0, 1.0, idleHSI[2] if idleHSI[2] != 0 else 1.0]
+        self.last_hsi = self.hsi
+    
+    def tick(self):
+        if self.last_hsi != idleHSI:
+            now = int(time.monotonic() * 1000)
+            if now > self.endNorm: # new norm has been set
+                self.endHue = now
+                self.endNorm = now + self.dt
+            
+            if now < self.endHue: # cycle hue
+                self.hsi[0] = (1 - cos( (now - self.iniHue) / float(self.endHue - self.iniHue) * math.pi ) ) / 2
+                self.last_hsi = self.hsi
+            elif now <= self.endNorm: # fade to norm
+                delta = (1 - cos( (now - self.endHue) / float(self.endNorm - self.endHue) * math.pi ) ) / 2
+                for i in range(len(self.hsi)):
+                    self.hsi[i] = (idleHSI[i] - self.last_hsi[i]) * delta + self.last_hsi[i]
+            else: 
+                self.last_hsi = idleHSI
+                self.hsi = idleHSI
+
--- VisualChimes/capacitiveChimes.py
+++ VisualChimes/capacitiveChimes.py
@@ -1,28 +1,23 @@
-import time
-import board
-import neopixel
-import busio
-from colorsys import hsv_to_rgb as hsi2rgb
-from .Ripple import Ripple, fxQ
-
 # Import MPR121 module.
-import adafruit_mpr121
+import time, board, neopixel, busio, geocoder, adafruit_mpr121
+from colorsys import hsv_to_rgb as hsi2rgb
+from .Ripple import *
+from skyfield import api
 # Create I2C bus.
 i2c = busio.I2C(board.SCL, board.SDA)
 # Create MPR121 object.
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
- 
 pixel_pin = board.A1
 num_pixels = 12
 # list of HSI colorspace data for each neopixel
-hsi = []
+p = []
+idleHSI = [0.0, 1.0, 1.0] # a global var to track idle color
 for i in range(num_pixels):
-    hsi.append([0.0, 1.0, 0.0])
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False,
-                           pixel_order=(1, 0, 2, 3))
+    p.append(Pixel)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False, pixel_order=(1, 0, 2, 3))
 # use this to set color data
-rgb = hsi2rgb(hsi[i][0], hsi[1], hsi[2])
+rgb = hsi2rgb(p.hsi[0], p.hsi[1], p.hsi[2])
 rgb = (round(rgb[0] * 255), round(rgb[1] * 255), round(rgb[2] * 255))
 pixels[i] = (rgb[0], rgb[1], rgb[2])
 # use this to display new color data
@@ -52,24 +47,3 @@ edges = 0; held = 0
 # do something to form queue of ripples
 # iterate existing queued fx jobs, then add new jobs to queue, then draw LEDs
 prevTouch = touch
-
-def go2norm(i):
-	if ((not effect.royGbiv and (hsi[i][0] >= 300/360.0)) or effect.royGbiv):
-		c = int(hsi[i][2] * 255)
-		if ((effect.royGbiv and c >= 128)):
-			hsi[i].setColorI(c-11) 
-		elif (int(c)):
-			hsi[i].setColorI(0)
-		else: return bool(int(c))
-	else: return False	
-
-# checks neopixels' hue value and increments (by 30 if hue <= 330) through spectrum
-def cycleHue(i):
-	tempH = int(hsi[i][0] * 360)
-	if tempH <= 330:
-		hsi[i][0] = (tempH + 30) / 360.0
-	else: hsi[i][0] = 0
-
-def setAllI(x): # where x == float [0,1]
-	for i in range(num_pixels):
-		hsi[i][2] = x
